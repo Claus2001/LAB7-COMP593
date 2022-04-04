506709b19740e003bb273905d7f7dfe307402102
Claus2001 <claus.delaguardia@flemingcollege.ca>
2022-04-04 12:14:25 -0400

    Update lab7script.py

--- lab7script.py
+++ lab7script.py
@@ -1,51 +1,51 @@
 def main():
 # Initialize the complex data structure
-    hockey_team_info = {
-        'name': 'Maple Leafs',
-        'city': 'Toronto',
-        'players': [
-            'Matthews',
-            'Marner',
-            'Tavares'
+    my_info = {
+        'name': 'Claus de la Guardia',
+        'student_id': '1234567890',
+        'pizza_toppings': [
+            'Bacon',
+            'Onion',
+            'Feta Chesse'
         ],
-        'games': [
-            {'opponent': 'Montreal',
+        'movies': [
+            {'genre': 'Sci-fi',
             'goals_for': 2,
             'goals_against': 4
             },
-            {'opponent': 'Florida',
-            'goals_for': 5,
+            {'genre': 'Comedy',
+            'The hangover' : 1,
             'goals_against': 2
             }
     ]
  }
    
     # Append new game to the end of the list
-    new_game = {'opponent': 'Boston', 'goals_for': 9, 'goals_against': 0}
-    hockey_team_info['games'].append(new_game)
+    #new_movie = {'opponent': 'Boston', 'goals_for': 9, 'goals_against': 0}
+    #my_info['games'].append(new_game)
    
     # Add a tuple of new players
     new_players = ('Nylander', 'Giordano', 'Campbell')
-    add_players(hockey_team_info, new_players)   
+    add_players(my_info, new_players)   
     # Print list of opponents team has played games against
-    print_teams_played(hockey_team_info)   
+    print_teams_played(my_info)   
 
-def print_teams_played(team_info):
+def print_teams_played(all_info):
     """
     Prints a sentence including a list of all teams played against.
    
     : param team_info: Team info data structure
     """
 # Create the first part of the sentence string
-    sentence = 'The '+ team_info['city'] + ' ' + team_info['name'] + ' has played games against '
-   
+    sentence = 'Hi jeremy, my name is '+ all_info['name'] + 'and my studend ID is ' + all_info['student_id'] + ' my ideal pizza has' + all_info['pizza_toppings']
+    print(sentence)
 # Iterate through every game in the data structure
-    for i,g in enumerate(team_info['games']): 
+    for i,g in enumerate(all_info['games']): 
 # Append the city of the opponent
         sentence += g['opponent'] + ","
    
 # Append appropriate punctuation
-    if i < len(team_info['games']) - 1:
+    if i < len(all_info['games']) - 1:
         sentence += ', '
     else:
         sentence += '.'
@@ -54,7 +54,7 @@ def print_teams_played(team_info):
     print(sentence)
    
    
-def add_players(team_info, players):
+def add_players(all_info, players):
     """
        Adds tuple (or list) of players to data structure
    
@@ -62,6 +62,6 @@ def add_players(team_info, players):
        : param player: tuple (or list) of players to add
        """
     for p in players:
-        team_info['players'].append(p)
+        all_info['players'].append(p)
  
 main()
